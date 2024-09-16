2e519ef7b6b444bacfaee73887d7edb539f0bde9
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2024-09-16 14:20:39 +0100

    Side bar button background update

--- src/Dashboard/index.jsx
+++ src/Dashboard/index.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
         <div className="w-[100vw] flex flex-row bg-[var(--ash-background)] md:min-h-[calc(100dvh-97.19px)] md:h-[100%] sm:p-[var(--custom-padding)] gap-[--custom-gap]">
           {url.pathname !== "/text-analytics" && <nav className='w-[10%]'>
             <ul className='flex flex-col gap-[calc(var(--custom-gap)/2)]'>
-              <li><Link className={'flex flex-row justify-between p-[var(--button-padding)] rounded-[0.5rem] font-semibold shadow-[var(--button-shadow)]' + (url.pathname === "/reports" && " bg-[var(--light-blue-background)]" || url.pathname === "/" && " bg-[var(--sidebarbtn-background)]")} to={"/reports"}><img src={AnalyticsIcon} className='w-[24px] h-[24px]'/>Reports</Link></li>
+              <li><Link className={'flex flex-row justify-between p-[var(--button-padding)] rounded-[0.5rem] font-semibold shadow-[var(--button-shadow)]' + (url.pathname === "/reports" && " bg-[var(--light-blue-background)]" || url.pathname === "/" && " bg-[var(--light-blue-background)]")} to={"/reports"}><img src={AnalyticsIcon} className='w-[24px] h-[24px]'/>Reports</Link></li>
               <li><Link className={'flex flex-row justify-between p-[var(--button-padding)] rounded-[0.5rem] font-semibold shadow-[var(--button-shadow)]' + (url.pathname === "/library" && " bg-[var(--light-blue-background)]")} to={"/library"}><img src={LibraryIcon} className='h-[24px]'/>Library</Link></li>
             </ul>
           </nav>}
